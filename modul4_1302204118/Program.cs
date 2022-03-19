c35e94ee4e6ec67d7046ee18d70d38aa1afc9912
Fiffah <101395835+Fiffah@users.noreply.github.com>
2022-03-19 14:01:37 +0700

    jurmod4

--- modul4_1302204118/Program.cs
+++ modul4_1302204118/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace mod4_1302204118
+{
+    class Program
+    {
+        static void Main(string[] args)
+    {
+        
+        SimpleDataBase<double> db = new SimpleDataBase<double>();
+        db.AddNewData(int);
+        db.AddNewData(int);
+        db.AddNewData(int);
+        db.PrintAllData();
+    }
+}
+
+class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        this.storedData = new List<T>();
+        this.inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T newData)
+    {
+        this.inputDates.Add(DateTime.Now);
+        this.storedData.Add(newData);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < this.inputDates.Count; i++)
+        {
+            Console.WriteLine("Data " + i + " berisi: " + this.storedData[i] +
+                ", yang disimpan pada waktu UTC: " + this.inputDates[i]);
+        }
+    }
+}
+}
