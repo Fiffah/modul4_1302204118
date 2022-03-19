5b523173b4407076128ae26d82db424e0882757b
Fiffah <101395835+Fiffah@users.noreply.github.com>
2022-03-19 14:04:17 +0700

    mod

--- modul4_1302204118/Program.cs
+++ modul4_1302204118/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace mod4_1302204118
+{
+    class Program
+    {
+        static void Main(string[] args)
+    {
+        
+        Console.WriteLine(Penjumlahan.JumlahTigaAngka<long>(12, 34, 56));
+
+        
+    }
+}
+
+class Penjumlahan
+{
+    public static T JumlahTigaAngka<T>(T input1, T input2, T input3)
+    {
+        dynamic a = input1;
+        dynamic b = input2;
+        dynamic c = input3;
+
+        return a + b + c;
+
+        
+    }
+}
+
+
+}
